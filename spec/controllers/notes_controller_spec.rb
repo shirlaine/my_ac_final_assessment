8487f9df0e0b70e4c7283008a82d7c0b95dd0c19
shirlaine <shirlaine.phang@gmail.com>
2017-11-29 20:08:50 +0800

    Add incomplete comtroller specs

---
+++ spec/controllers/notes_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Users::NotesController, type: :controller do
+
+  let(:user1) { create(:user) }
+
+  before { sign_in user1 }
+
+  describe "GET #index" do
+    let(:user_1) { create(:user) }
+    let!(:note_1) { create(:note, user: user_1) }
+
+    before do
+      get :index
+    end
+
+    # it { expect(assigns(:notes).count).to eq(1) }
+    # it { expect(assigns(:notes)).to eq(note_1) }
+    it { expect(Note.count).to eq(1)}
+  end
+
+  describe "GET #show" do
+
+    # let(:user1) { create(:user) }
+    let!(:note_1) { create(:note) }
+
+     before { get :show, params: { id: note_1.id } }
+
+    it { expect(assigns(:note)).to eq(note_1) }
+  end
+
+
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    username 'user1'
+    sequence :email do |n|
+      "person#{n}@example.com"
+    end
+    password '123123123'
   end
 end
