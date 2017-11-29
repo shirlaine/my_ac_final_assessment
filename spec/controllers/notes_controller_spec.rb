6c2055301f3a725a54881b378a2e793999a86090
shirlaine <shirlaine.phang@gmail.com>
2017-11-29 21:13:11 +0800

    Update controller specs

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -7,21 +7,19 @@ RSpec.describe Users::NotesController, type: :controller do
   before { sign_in user1 }
 
   describe "GET #index" do
-    let(:user_1) { create(:user) }
-    let!(:note_1) { create(:note, user: user_1) }
+    let!(:note_1) { create(:note, user: user1) }
 
     before do
       get :index
     end
 
-    # it { expect(assigns(:notes).count).to eq(1) }
+    it { expect(assigns(:notes).count).to eq(1) }
     # it { expect(assigns(:notes)).to eq(note_1) }
-    it { expect(Note.count).to eq(1)}
+    # it { expect(Note.count).to eq(1)}
   end
 
   describe "GET #show" do
 
-    # let(:user1) { create(:user) }
     let!(:note_1) { create(:note) }
 
      before { get :show, params: { id: note_1.id } }
