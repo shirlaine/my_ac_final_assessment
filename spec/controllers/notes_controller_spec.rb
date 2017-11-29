4c45231c44b9d49db878b232e26f0a09bff83a0f
shirlaine <shirlaine.phang@gmail.com>
2017-11-29 21:56:08 +0800

    Update note_controller_specs

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Users::NotesController, type: :controller do
   before { sign_in user1 }
 
   describe "GET #index" do
+
     let!(:note_1) { create(:note, user: user1) }
 
     before do
@@ -25,7 +26,56 @@ RSpec.describe Users::NotesController, type: :controller do
      before { get :show, params: { id: note_1.id } }
 
     it { expect(assigns(:note)).to eq(note_1) }
+
   end
+
+  describe 'GET #new' do
+
+    before { get :new, params: { user_id: user1 } }
+
+    it { expect(assigns(:note)).to be_a_new(Note) }
+
+  end
+
+  describe 'GET #edit' do
+
+    let!(:note_1) { create(:note, user: user1) }
+
+    before { get :edit, params: { user_id: user1, id: note_1 } }
+
+    it { expect(assigns(:note)).to eq(note_1) }
+
+  end
+
+  describe 'POST #create' do
+
+    before do
+      post :create, params: {
+        user_id: user1,
+        note: note_attributes
+      }
+    end
+
+    context 'when save is successful' do
+
+      let(:note_attributes) { attributes_for(:note) }
+      let(:note_1) { create(:note) }
+
+      it { expect(Note.count).to eq(1) }
+      it { expect(subject).to redirect_to(users_notes_path) }
+
+    end
+
+    context 'when save is unsuccessful' do
+
+      let(:note_attributes) { attributes_for(:note, title: nil) }
+
+      it { expect(subject).to render_template(:new) }
+
+    end
+
+  end
+
 
 
 end
