43db3d48be3c80df053cfd51722b4babddff0790
shirlaine <shirlaine.phang@gmail.com>
2017-11-29 12:18:42 +0800

    Change root path for logged in vs logged out users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticated :user do
+    root 'users/notes#index', as: :authenticated_root
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'notes#index'
   resources :notes, only: [:index]
