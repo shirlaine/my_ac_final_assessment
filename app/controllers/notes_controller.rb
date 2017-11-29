1ff745d8b5d861d5a1e9201f07c16b5cb947c0a7
shirlaine <shirlaine.phang@gmail.com>
2017-11-29 11:25:56 +0800

    Add edit and update action and views, and destroy action to notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,11 +1,11 @@
 class NotesController < ApplicationController
+  before_action :prepare_note, only: [:show, :edit, :update, :destroy]
 
   def index #show all users im following and list of notes
     @notes = Note.all
   end
 
   def show
-    @note = Note.find(params[:id])
   end
 
   def new
@@ -23,16 +23,33 @@ class NotesController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+  end
 
-  def update; end
+  def update
+    if @note.update(notes_params)
+      flash["notice"] = "Your note has been updated!"
+      redirect_to notes_path
+    else
+      flash["alert"] = "Your note was not updated"
+      render :edit
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @note.destroy
+    flash["notice"] = "Your note has destroyed!"
+    redirect_to notes_path
+  end
 
 private
 
   def notes_params
     params.require(:note).permit(:title, :body)
+  end
+
+  def prepare_note
+    @note = Note.find(params[:id])
   end
 
 end
